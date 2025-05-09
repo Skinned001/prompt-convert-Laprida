3115c7fea501f04ef7768760d19e040f6ef907fd
Fernando <psamathe001@gmail.com>
2025-05-09 12:54:47 -0300

    base de los scripts

---
+++ app.js
@@ -0,0 +1,15 @@
+let valor = prompt ("ingrese 1 para el conversor, 2 para salir");
+function conversor(currency (pesos){
+    return pesos/1300;
+}
+switch(valor) {
+    case "1":
+      let pesos = prompt("Ingrese el valor en pesos");
+      let calculo
+      break;
+    case y:
+      // code block
+      break;
+    default:
+      // code block
+  }
