f1fb1288741e48f7112bd1480240f2dc11a39dce
Fernando <psamathe001@gmail.com>
2025-05-12 09:58:35 -0300

    Pagina funcional

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-let valor ;
-let historia = [];
+const history = [];
+const dolarActual = 1300;
 let salir = false;
 
-function conversor_currency (pesos){
-    return pesos/1300;
+function convertCurrency (pesos){
+    return pesos/dolarActual;
 }
 
 
 while (salir == false) {
   let option = prompt(
-    "Seleccione una opcion: \n1) Divisa ARS->USD \n2 Salir"
+    "Seleccione una opcion: \n1) Convertir Divisas ARS->USD \n2 Salir"
   );
   switch (option) {
     case "1":
       let pesos = prompt("INGRESE LOS PESOS A CONVERTIR");
-      let conversion = conversor_currency(pesos);
-      historia.push({
+      let conversion =  convertCurrency(pesos);
+      history.push({
         tipo: "peso",
         entrada: pesos,
-        salida: conversion,
+        salida: conversion.toFixed(2),
         fecha: new Date().toLocaleDateString() 
       });
       break;
     case "2":
-      salir == true;
-        for (let i in historia){
-          console.log ("Moneda ingresa es: ",historia[i].tipo);
-          console.log ("Valor en pesos es: ",historia[i].entrada);
-          console.log ("Valor en USD es: ",historia[i].salida);
-          console.log ("Fecha es: ",historia[i].fecha);
+      salir = true;
+        for (let i in history){
+          console.log ("Moneda ingresa es de : ",history[i].tipo);
+          console.log ("Monto en pesos es de : ",history[i].entrada);
+          console.log ("Monto en USD es de : ",history[i].salida);
+          console.log ("Fecha de la consulta es : ",history[i].fecha);
         }
+        alert(`El resultado es: ${convertCurrency(pesos).toFixed(2)} dólares`);
         break;
+            default:
+        prompt("Ingrese una opcion valida - ingrese una tecla para continuar");
   }
 }
 
